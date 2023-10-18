a70bad34697abdc4cb3ba5e642c36eab0331d8c1
zhangyutong <zhangyutong@sailyond.com>
2023-10-18 11:52:51 +0100

    first commit

---
+++ WiFi_module/WiFi_module.ino
@@ -0,0 +1,13 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "CE-Hub-Student";
+const char* password = "casa-ce-gagain-public-service";
+const char* host = "iot.io";
+
+void setup() {    
+
+}
+
+void loop() {    
+
+}
